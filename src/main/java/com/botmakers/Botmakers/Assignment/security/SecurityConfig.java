5a5f02ece4068cfce175741ba47826302724851f
abhi0399 <abhi0399>
2026-03-16 23:38:42 +0530

    Initial commit

--- src/main/java/com/botmakers/Botmakers/Assignment/security/SecurityConfig.java
+++ src/main/java/com/botmakers/Botmakers/Assignment/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/api/public/**",
+                                "/api/auth/register/**",
                                 "/h2-console/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
